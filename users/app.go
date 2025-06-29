b6f0eb8275a0b707e8f9f6d941c56f8fe35165f5
Ahamed Rujhan <rujhanbheem123@gmail.com>
2025-06-29 16:28:10 +0530

    Price calculator inital setup

--- users/app.go
+++ users/app.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func getUserData(printText string) string {
 	fmt.Println(printText)
@@ -9,10 +12,38 @@ func getUserData(printText string) string {
 	return inputText
 }
 
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
 func main() {
 	firstName := getUserData("please enter your first name")
 	lastName := getUserData("please enter your last name")
 	birthDate := getUserData("please enter your birth of date")
+
+	var appUser user
+
+	// struct literal notation
+	// we can omit any value if we want
+	//appUser = user{
+	//	firstName: firstName,
+	//	lastName:  lastName,
+	//	birthDate: birthDate,
+	//	createdAt: time.Now(),
+	//}
+
+	// in this method order is important
+	appUser = user{
+		firstName,
+		lastName,
+		birthDate,
+		time.Now(),
+	}
+	//null value struct
+	//appUser = user{}
 
 	// do sth...
 
